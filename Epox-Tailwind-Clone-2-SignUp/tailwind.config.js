00c8d07fade9ba3a6fc864a19eb42d9da87a3b08
Berke Udunman <61910163+brkeudunman@users.noreply.github.com>
2022-05-04 17:27:25 +0300

    Add files via upload

---
+++ Epox-Tailwind-Clone-2-SignUp/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'montserrat': ['Montserrat'],
+        'lato': ['Lato'],
+        'garamond': ['Garamond']
+    }
+    },
+  },
+  plugins: [],
+}
