560a7f92a2f1a08cd3caf49c7b4c06be48f7544a
Berke Udunman <61910163+brkeudunman@users.noreply.github.com>
2022-05-17 11:07:21 +0300

    Add files via upload

---
+++ web&mobile-page1/tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'montserrat': ['Montserrat'],
+        'lato': ['Lato'],
+        'garamond': ['Garamond']
+    },
+    colors:{
+      'white': '#ffffff',
+      'purple-test': '#5424F3',
+      'bone-test': '#F6F9FC',
+      'metal-test': '#999999',
+      'darkblue-test': '#3D4456',
+      
+    },
+    boxShadow: {
+      'test': '0px 2px 2px rgba(0, 0, 0, 0.15)',
+    }
+  }
+  },
+  plugins: [],
+}
